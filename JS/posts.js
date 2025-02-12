c18ee71a0791b12064955de262fa653039ec4f4a
edinson0810 <lache081015@gmail.com>
2025-02-11 21:16:16 -0500

    falta mostrar los posts

--- JS/posts.js
+++ JS/posts.js
@@ -1,6 +1,10 @@
 export const posts = async () => {
-    let data = await fetch('https://jsonplaceholder.typicode.com/users${id}/posts')
+    let data = await fetch('https://jsonplaceholder.typicode.com/users/${id}/posts')
     let response = await data.json();
     return response;
     
     }
+
+
+
+    
