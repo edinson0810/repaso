a88231c6dadf8b445d3d959654795fd7788679cd
edinson0810 <lache081015@gmail.com>
2025-02-11 21:13:03 -0500

    repaso

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,42 @@
 import { users } from "./users.js";
+import { posts } from "./posts.js";
 
-users().then((ok) => {
-    console.log(ok);
-    
-});
 
+const root = document.getElementById(`root`);
+
+
+const imprimir = async() => {
+
+    try {
+        const usuario = await users();
+        await Promise.all(usuario.map(async ({name,email,id}) => {
+
+            let div = document.createElement(`div`);
+            let htmlName = document.createElement(`div`);
+            let htmlEmail = document.createElement(`div`);
+            let htmlButton = document.createElement(`button`);
+            htmlName.innerText = name;
+            htmlEmail.innerText = email;
+            htmlButton.innerText = "cuantos Post"
+
+            div.append(htmlName,htmlEmail, htmlButton)
+            div.classList.add(`cards`);
+
+            root.appendChild(div);
+            
+          htmlButton.addEventListener(`click`, () => {
+            // alert(`el usuario con id ${usuario.id}`);
+            posts(id).then(data => {
+                alert(`el usuario ${name} tiene tantos ${data.length} posts`)
+            })
+          })
+            
+        }));
+
+    } catch  {
+        console.error("Error");
+    }
+}
+
+
+imprimir();
---
+++ JS/posts.js
@@ -0,0 +1,6 @@
+export const posts = async () => {
+    let data = await fetch('https://jsonplaceholder.typicode.com/users${id}/posts')
+    let response = await data.json();
+    return response;
+    
+    }
